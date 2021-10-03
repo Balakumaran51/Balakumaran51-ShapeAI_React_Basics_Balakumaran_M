bf281cd7b34c0a5987a64e08b2f1b09becc40a09
Balakumaran51 <84306178+Balakumaran51@users.noreply.github.com>
2021-10-03 15:32:53 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Toolbar, Typography} from '@material-ui/core';
 import {makeStyles} from "@material-ui/core/styles";
+import  {BrowserRouter as Router, Link} from 'react-router-dom';
+
 const styles = makeStyles({
     bar:{
         paddingTop: "1.15rem",
@@ -18,27 +20,41 @@ const styles = makeStyles({
         },
         ['@media (max-width:780px)']: { 
             paddingBottom: "1rem"    }
-    }
-})
-
-function NavBar() {
+        },
+        dec:{
+            textDecoration:"none",
+            color: "white",
+            "&:hover": {
+                color:  "rgb(79,37,247)"
+            },
+        }
+    })
+    function NavBar() {
     const classes = styles()
     return (
             <Toolbar position="left" color="rgba(0, 0, 0, 0.87)" className={classes.bar}> 
                 <Typography variant="h6" className={classes.menuItem}>
-                   About Me
+                About Me
                 </Typography>
                 <Typography variant="h6" className={classes.menuItem}>
-                    Resume
+                <Link to='Resume' className={classes.dec}>   
+                Resume
+                </Link>
                 </Typography>
                 <Typography variant="h6" className={classes.menuItem}>
-                    Projects
+                <Link to='Projects' className={classes.dec}>   
+                Projects
+                </Link>
                 </Typography>
                 <Typography variant="h6" className={classes.menuItem}>
-                    Skills 
+                <Link to='Skills' className={classes.dec}>   
+                Skills
+                </Link> 
                 </Typography>
                 <Typography variant="h6" className={classes.menuItem}>
-                    Contact Me 
+                <Link to='Footer' className={classes.dec}>   
+                Contact Me
+                </Link> 
                 </Typography>
             </Toolbar>
     )
